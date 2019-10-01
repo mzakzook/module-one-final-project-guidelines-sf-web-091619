7af37304267e89462d936f4dfe1cfa1cdbf8e8cb
Max Zakzook <mzakzook@me.com>
2019-10-01 15:08:02 -0700

    fixed environment

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 source 'https://rubygems.org'
-gem 'activerecord', '~> 5.2', require: 'active_record'
+gem 'activerecord', '~> 5.2'
 gem 'database_cleaner'
 gem 'pry'
 gem 'rake'
@@ -10,4 +10,4 @@ gem 'sinatra-activerecord', require: 'sinatra/activerecord'
 gem 'sqlite3', '~> 1.3.6'
 gem 'tty'
 gem 'faker'
-
+gem 'rest-client'
--- config/environment.rb
+++ config/environment.rb
@@ -6,9 +6,10 @@ Bundler.require
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require 'bundler/setup'
 require 'sinatra/activerecord'
-Bundler.require
+
 Dir[File.join(File.dirname(__FILE__), '../app/models', '*.rb')].each { |f| require f }
 connection_details = YAML.safe_load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
 
 require_all 'app'
+require_all 'lib'
