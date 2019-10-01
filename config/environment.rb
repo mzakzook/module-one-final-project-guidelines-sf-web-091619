ccaf9d2028c5e0e8dc50330c69c16a8b8ece7ebf
Max Zakzook <mzakzook@me.com>
2019-10-01 13:19:03 -0700

    user seeds created

--- config/environment.rb
+++ config/environment.rb
@@ -1,26 +1,14 @@
-
+require 'bundler'
 require 'rake'
 require 'active_record'
-require 'bundler'
+require 'faker'
 Bundler.require
-
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
-
 require 'bundler/setup'
 require 'sinatra/activerecord'
-
+Bundler.require
 Dir[File.join(File.dirname(__FILE__), '../app/models', '*.rb')].each { |f| require f }
 connection_details = YAML.safe_load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details)
-# uncomment line 23 and comment out line 22 in order to show all of the
-# SQL queries that Active Record handles.
-ActiveRecord::Base.logger = nil
-# ActiveRecord::Base.logger = Logger.new(STDOUT)
+
 require_all 'app'
-
-
-
-
-
-
